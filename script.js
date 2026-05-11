e3ddd2e4a0c78b11a7c5382d3353a430f70f0e3c
NOUSHERNISHAD <mdnishad99100gmail.com>
2026-05-11 23:13:37 +0600

    mainhtml half half html code done

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('co');
+//  login button functionality
+document.getElementById("loginButton")
+    .addEventListener("click",function(e){
+        e.preventDefault()
+
+        const mobileNumber = 18224582430
+        const pinNumber = 1234
+
+        const mobileNumberValue = document.getElementById("mobile-number").value
+        const mobileNumberValueConverted = parseInt(mobileNumberValue)
+        // console.log("mobileNumberValueConverted");
+
+        const pinNumberValue = document.getElementById("pin-number").value
+        const pinNumberValueConverted = parseInt(pinNumberValue)
+
+        console.log(mobileNumberValueConverted, pinNumberValueConverted);
+
+        if (mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber) {
+            // console.log('all matched');
+            alert('Login Successfully')
+            window.location.href = "./main.html"
+
+        }
+        else {
+            // console.log('invalid number');
+            alert('Invalid Password')
+            alert('Please fill in the blanks correct number')
+
+        }
+
+
+
+    })
